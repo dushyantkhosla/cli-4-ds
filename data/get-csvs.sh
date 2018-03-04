4860b5b5309dcb9e1596f4f24ee71ba6c9a875d4
Dushyant Khosla <dushyant.khosla@pmi.com>
2018-03-04 16:14:43 +0100

    Conda now works with fish

--- data/get-csvs.sh
+++ data/get-csvs.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-
+echo "-----------------------------------------------------------------"
 echo "Downloading the KDD Cup 1999"
+echo "-----------------------------------------------------------------"
 wget http://kdd.ics.uci.edu/databases/kddcup99/kddcup.names
 wget http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data.gz
 
+echo "-----------------------------------------------------------------"
 echo "Uncompressing KDD Data"
+echo "-----------------------------------------------------------------"
 dtrx kddcup.data.gz
 
+echo "-----------------------------------------------------------------"
 echo "Downloading the Flights Data"
+echo "-----------------------------------------------------------------"
 wget http://stat-computing.org/dataexpo/2009/2007.csv.bz2
 
+echo "-----------------------------------------------------------------"
 echo "Uncompressing KDD Data"
+echo "-----------------------------------------------------------------"
 dtrx 2007.csv.bz2
 
+echo "-----------------------------------------------------------------"
 echo "Done. Removing the compressed files"
-
+echo "-----------------------------------------------------------------"
 rm 2007.csv.bz2
 rm kddcup.data.gz
--- docker/install-fish.sh
+++ docker/install-fish.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
 echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/2/Debian_8.0/ /' >> /etc/apt/sources.list.d/fish.list
 apt-get update
 wget -qO - http://download.opensuse.org/repositories/shells:fish:release:2/Debian_8.0/Release.key | apt-key add -
 apt-get update
 apt-get install -y fish
+
+mkdir /root/.config/
+touch /root/.config/config.fish
+echo "source (conda info --root)/etc/fish/conf.d/conda.fish" > ~/.config/config.fish
