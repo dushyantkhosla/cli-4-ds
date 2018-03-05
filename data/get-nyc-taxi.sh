e6d15393698ddc79968bdb01d7480b0f5d8052fb
Dushyant Khosla <dushyant.khosla@pmi.com>
2018-03-05 15:57:49 +0100

    Added script for downloading NYC taxi data

---
+++ data/get-nyc-taxi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir nyc-taxi
+cd nyc-taxi
+wget 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2017-01.csv'
+wget 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2017-02.csv'
+wget 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2017-03.csv'
+wget 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2017-04.csv'
+wget 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2017-05.csv'
+wget 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2017-06.csv'
+wget 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2017-07.csv'
+wget 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2017-08.csv'
+wget 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2017-09.csv'
+wget 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2017-10.csv'
+wget 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2017-11.csv'
+wget 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2017-12.csv'
